f7d14ceff27e5b7cb148fb53ad1a44a7f61cde7a
Benjamin A. Laken <benlaken@gmail.com>
2016-07-01 11:40:23 +0100

    change to name and more structure added

---
+++ DashPykpi/kpistats.py
@@ -0,0 +1,6 @@
+def example_foo(input=None):
+    """A foo function to check we are up and running.
+    :param None input: The function input
+    :param None output: The function returns None
+    """
+    return input
---
+++ DashPykpi/test/test_kpistats.py
@@ -0,0 +1,5 @@
+from DashPykpi import example_foo
+
+def test_foo():
+    val = example_foo()
+    assert val is None
