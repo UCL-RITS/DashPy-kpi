d4cf977d2cdf0b9955132a931e051c6f8e7cb6fa
Benjamin A. Laken <benlaken@gmail.com>
2016-07-11 13:34:48 +0100

    added Login() function again

--- DashPykpi/kpistats.py
+++ DashPykpi/kpistats.py
@@ -6,16 +6,25 @@ import getpass
 import json
 import requests
 
-class KpiStats_dev(gh_auth):
+
+class KpiStats_dev(object):
     """Gather key statstics from a repo url.
     """
     def __init__(self, url):
+        gh_name = input("Username to access github with:")
+        self.gh_name
+        # nb. this login can be changed to use a github token
+        self.gh = login(gh_name, getpass.getpass(prompt='Ghub psswd of {0}:'.format(gh_name)))
         self.url = url
         self.repo = None
         self.stats = None
 
+    def __str__(self):
+        print("KPI stats back-end")
+
     def get_repo_object_from_url(self):
-        assert type(url) == str,"Error: url should be a string in format of 'https://github.com/<user>/<repo>'"
+        demo = 'https://github.com/<user>/<repo>'
+        assert type(url) == str, "Error: url should be a string in format of " + demo
         assert url.split('/')[-3] == 'github.com', "Error: {0} isn't valid ".format(url)
         user_str, repo_str = url.split('/')[-2:]
         self.repo = self.gh.repository(user_str, repo_str)
@@ -34,7 +43,7 @@ class KpiStats_dev(gh_auth):
             'repo_url': self.repo.clone_url,
             }
         return
-    
+
     def work(self):
         self.get_repo_object_from_url()
         self.get_repo_stats()
