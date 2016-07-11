4c794b2266153e20c68ff726347cef663704b793
Benjamin A. Laken <benlaken@gmail.com>
2016-07-11 15:01:53 +0100

    testing environment variable in Travis is accessible to code

--- DashPykpi/test/test_kpistats.py
+++ DashPykpi/test/test_kpistats.py
@@ -1,8 +1,21 @@
 from DashPykpi.kpistats import KpiStats
+import os
+
+
+def test_public_repo_access():
+    url = "https://github.com/benlaken/Comment_BadruddinAslam2014"  # 21commits
+    test = KpiStats(url)
+    test.work()
+    assert test.stats['total_commits'] > 20
 
 
 def test_private_repo_access():
-    url = "https://github.com/UCL-RITS/RSD-Dashboard"# >1k commits + PRIVATE
+    url = "https://github.com/UCL-RITS/RSD-Dashboard"  # >1k commits + PRIVATE
     test = KpiStats(url)
     test.work()
     assert test.stats['total_commits'] > 1000
+
+
+def test_environment_variable():
+    print(os.environ['TEST_VAR'])
+    assert os.environ['TEST_VAR'] == 'TEST_192837'
