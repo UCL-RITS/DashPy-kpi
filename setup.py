0fecf5f7e34b6fdc5b16f0d7d103c21cfa70a74a
Raquel Alegre <r.alegre@ucl.ac.uk>
2016-09-13 15:48:37 +0100

    Moved one sub version up.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="DashPykpi",
-    version="0.1",
+    version="0.1.1",
     description='Functions to create KPI info for RSD Dashboard from Github',
     url='https://github.com/UCL-RITS/DashPy-kpi',
     author='Benjamin A. Laken',
